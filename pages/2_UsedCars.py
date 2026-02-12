82cd8c8667d3aa464e7925a91a7f2dbd322ad07d
Martin Kagema <martinkagema01@gmail.com>
2026-02-12 11:19:43 +0300

    Additional Commits

--- pages/2_UsedCars.py
+++ pages/2_UsedCars.py
@@ -2,36 +2,81 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_extras.dataframe_explorer import dataframe_explorer
+from langchain_experimental.agents import create_csv_agent
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
 
-st.set_page_config(
-    layout='wide'
-)
+#-------------Page Config---------
+st.set_page_config(layout='wide')
 st.title='UAE Used Cars Market Analysis'
 
+#-------------Load Data-----------
 df = pd.read_csv('dataset/UsedCars.csv')
 
+#-------------Analytics Section-------
 st.subheader('Detailed Analysis of Used Cars')
-#st.dataframe(df,use_container_width=True)
+
 filtered_df = dataframe_explorer(df, case=True)
-st.dataframe(filtered_df)
+st.dataframe(filtered_df,use_container_width=True)
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.subheader('Mileage VS Price', divider='rainbow')
     fig = px.scatter(
-        df,
+        filtered_df,
         x='Mileage',
         y='Price',
-        color='Model'
+        color='Make',
+        hover_data=['Model','Year']
     )
     st.plotly_chart(fig,use_container_width=True)
 
 with col2:
     st.subheader('Most Common Car Brands')
+    brand_counts = (
+        filtered_df['Make'].
+        value_counts().
+        reset_index().
+        rename(columns={'index':'Make','Make':'Count'})
+    )
     fig2 = px.bar(
-        df['Make'].value_counts().reset_index(),
-        x='Year',
-        y='Make'
+        brand_counts,
+        x='Make',
+        y='Count'
     )
     st.plotly_chart(fig2, use_container_width=True)
+
+#-----------AI Interaction Section-----------
+st.divider()
+st.subheader('🤖 Ask the Data(AI-Powered Insights)')
+
+st.markdown(
+    """
+Ask questions such as:
+- Which car brands offer the best value for money?
+- How does mileage affect resale price?
+- What year range has the best price-to-mileage ratio?
+- Which cars are overpriced for their mileage?
+- Are newer cars always more expensive?
+"""
+)
+
+#------------Langchain Setup-----------
+load_dotenv()
+api_key = os.getenv('GROQ_API_KEY')
+llm = ChatGroq(model='llama-3.1-8b-instant',temperature=0.6,api_key=api_key)
+
+user_csv = 'dataset/UsedCars.csv'
+
+chatcsv = create_csv_agent(llm,user_csv,allow_dangerous_code=True,verbose=True)
+
+#--------------Chat Input----------------
+user_question = st.chat_input('Ask a question about used cars market')
+
+if user_question:
+    with st.spinner('Analyzing the data...'):
+        response = chatcsv.run(user_question)
+    st.markdown('### AI Insight')
+    st.write(response)
--- pages/3_CarBuyers.py
+++ pages/3_CarBuyers.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
 from streamlit_extras.dataframe_explorer import dataframe_explorer
+from langchain_experimental.agents import create_csv_agent
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os 
 
+
+#-----------Page Config---------
 st.set_page_config(layout='wide')
-
 st.title='Car Buyers Market Behaviour'
+
+#-----------Load Data------------
 df=pd.read_csv('dataset/CarBuyers.csv')
 
+#------------Analytics Section------
 st.subheader('List of Car Buyers with their Car Preferences')
-#st.dataframe(df,use_container_width=True)
 
 filtered_df = dataframe_explorer(df,case=True)
 st.dataframe(filtered_df,use_container_width=True)
@@ -19,39 +26,77 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader('Male Car Preferences',divider='rainbow')
     fig = px.bar(
-        df,
+        filtered_df,
         x='Model',
         y='Male',
+        title = 'Male Buyer Preference by Model'
     )
     st.plotly_chart(fig,  use_container_width=True)
 
 with col2:
     st.subheader('Female Car Preferences',divider='rainbow')
     fig1 = px.bar(
-        df,
+        filtered_df,
         x='Model',
         y='Female',
+        title = 'Female Buyer Preference by Model'
     )
     st.plotly_chart(fig1,  use_container_width=True)
 
 col11, col22 = st.columns(2)
 
 with col11:
-    st.subheader('Buyer Intent', divider='rainbow')
-    fig2 = px.scatter(
-        df,
+    st.subheader('Male Buyer Price Sensitivity', divider='rainbow')
+    fig2 = px.bar(
+        filtered_df,
         x='Model',
         y='Price',
-        color='Male'
+        color='Male',
+        title='Price vs Male Buyer Interest'
     )
     st.plotly_chart(fig2,use_container_width=True)
 
 with col22:
-    st.subheader('Buyer Intent', divider='rainbow')
-    fig3 = px.scatter(
-        df,
+    st.subheader('Female Buyer Price Sensitivity', divider='rainbow')
+    fig3 = px.bar(
+        filtered_df,
         x='Model',
         y='Price',
-        color='Female'
+        color='Female',
+        title='Price vs Female Buyer Interest'
     )
     st.plotly_chart(fig3,use_container_width=True)
+
+#---------------AI Interaction Section-----------
+st.divider()
+st.subheader(' Ask the Data(AI-Powered Insights)')
+
+st.markdown(
+    """
+Try questions like:
+- Which car models are preferred more by male buyers?
+- Which models attract both male and female buyers?
+- Are higher-priced cars preferred by a specified gender?
+- Which models show balanced buyer interest?
+- What pricing strategy fits each buyer segment?
+"""
+)
+
+#-----------Langchain Setup-------------
+load_dotenv()
+api_key = os.getenv('GROQ_API_KEY')
+llm = ChatGroq(model='llama-3.1-8b-instant', temperature=0.6,api_key=api_key)
+
+user_csv= 'dataset/CarBuyers.csv'
+
+chatcsv = create_csv_agent(llm,user_csv,allow_dangerous_code=True,verbose=True)
+
+
+#-------------Chat Input-----------
+user_question = st.chat_input('Ask a question about Car Buyers')
+
+if user_question:
+    with st.spinner('Analyzing buyer behavoir...'):
+        response = chatcsv.run(user_question)
+    st.markdown('### AI Insight')
+    st.write(response)
