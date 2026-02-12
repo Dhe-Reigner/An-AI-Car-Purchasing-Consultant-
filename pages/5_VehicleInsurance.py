b7ce3acb110b18f5d15643f01e226e74a447ab75
Martin Kagema <martinkagema01@gmail.com>
2026-02-12 13:53:11 +0300

    Additional Commits

--- pages/5_VehicleInsurance.py
+++ pages/5_VehicleInsurance.py
@@ -2,14 +2,20 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_extras.dataframe_explorer import dataframe_explorer
+from langchain_groq import  ChatGroq
+from langchain_experimental.agents import create_csv_agent
+from dotenv import load_dotenv
+import os
 
+#----------Page Config--------
 st.set_page_config(layout='wide')
-
 st.title='Vehicle Insurance Cost Analysis'
 
+#------------Load Data-----------
 df = pd.read_csv('dataset/VehicleInsurance.csv')
+
+#---------Analytics Section--------
 st.subheader('Handling Vehicle Insurance')
-#st.dataframe(df, use_container_width=True)
 
 filtered_df = dataframe_explorer(df,case=True)
 st.dataframe(filtered_df, use_container_width=True)
@@ -19,7 +25,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader('Insurance Premium by Vehicle Type')
     fig = px.bar(
-        df,
+        filtered_df,
         x='TYPE_VEHICLE',
         y='PREMIUM'
     )
@@ -28,8 +34,37 @@ with col1:
 with col2:
     st.subheader('Risk Category VS Premium', divider='rainbow')
     fig2 = px.box(
-        df,
+        filtered_df,
         x='USAGE',
         y='PREMIUM'
     )
     st.plotly_chart(fig2,use_container_width=True)
+
+#--------------AI Interaction Section-----------
+st.divider()
+st.subheader(' Ask the Data(AI-Powered Insights)')
+
+st.markdown(
+    """
+"""
+)
+
+
+#------------Langchain Setup--------
+load_dotenv()
+api_key = os.getenv('GROQ_API_KEY')
+llm = ChatGroq(model='llama-3.1-8b-instant',temperature=0.6,api_key=api_key)
+
+user_csv = 'dataset/VehicleInsurance.csv'
+
+chatscv = create_csv_agent(llm, user_csv,allow_dangerous_code=True,verbose=True)
+
+#----------Chat Input--------
+
+user_question = st.chat_input('Ask a questo=ion about Vehicle Insurance')
+
+if user_question:
+    with st.spinner('Analyzing Insurance provision... '):
+        response = chatscv.run(user_question)
+    st.markdown('### AI Insights')
+    st.write(response)
