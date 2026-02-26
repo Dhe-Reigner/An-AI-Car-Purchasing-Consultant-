5da74793e20e7805db9b4ac158d9b8cc4b15832e
Martin Kagema <martinkagema01@gmail.com>
2026-02-26 15:49:28 +0300

    Reccomendation Engine

--- pages/7_CarPurchase_Recommendation.py
+++ pages/7_CarPurchase_Recommendation.py
@@ -43,7 +43,71 @@ Welcome to your intelligent car buying assistant.
             employment_status = st.selectbox('🧑‍💼Employment Type',['Employed','Self-Employed','Business Owner'])
 
             loan_term = st.selectbox('🏦Preferred Loan Term (Years)',[1,3,5,7])
-            budget = st.number_input('🚘Car Condition Preference',['New','Used','Either'])
+            budget = st.number_input('💰Budget for Car ($)',min_value=0.0, step=1000.0)
+            car_condition = st.number_input('🚘Car Condition Preference',['New','Used','Either'])
+
+        st.header('Vehicle Preferences')
+
+        col3, col4 = st.columns(2)
+
+        with col3:
+            transmission = st.selectbox('⚙️ Transmission Type',['Automatic','Manual','Any'])
+            fuel_type = st.selectbox('⛽Fuel Type',['Petrol','Diesel','Hybrid','Electric','Any'])
+            body_type = st.selectbox('🚙Body Type',['Sedan','SUV','Hatchback','Truck','Coupe','Any'])
+            seats = st.number_input('🪑 Minimum Number of seats',min_value=2,max_value=10,step=1)
+
+        with col4:
+            usage = st.selectbox('🛣️Primary Usage',['Personal','Family','Business','Commercial'])
+            mileage_preference = st.selectbox('📉Mileage Preference'['Low Mileage','Moderate','No Preference'])
+            insurance_sensitivity = st.selectbox('🛡️Insurance Cost Sensitivity',['Low Premium Preferred','Not a Concern'])
+            reliability_priority = st.selectbox('🛠️Reliability Importance',['Very Important','Moderate','Not a Priority'])
+        
+        if st.button('🔍 Generate Car Recommendation'):
+            required_fields = [
+                name,age,gender,country,annual_salary,net_worth,cibil_score,budget,transmission,fuel_type
+            ]
+
+            if not all(required_fields):
+                st.error('Please fill in all required fileds to generate recommendation')
+            else:
+                inputs = {
+                    'name':name,
+                    'age':age,
+                    'gender':gender,
+                    'annual_salary':annual_salary,
+                    'credit_card_debt':credit_card_debt,
+                    'net_worth':net_worth,
+                    'credit_score':cibil_score,
+                    'dependents':dependents,
+                    'employment_status':employment_status,
+                    'loan_term':loan_term,
+                    'budget':budget,
+                    'car_condition':car_condition,
+                    'transmission':transmission,
+                    'fuel_type':fuel_type,
+                    'body_type':body_type,
+                    'seats':seats,
+                    'usage':usage,
+                    'mileage_preference':mileage_preference,
+                    'insurance_sensitivity':insurance_sensitivity,
+                    'reliability_priority':reliability_priority
+                }
+
+                with st.spinner('Analyzing financial profile, market trends,insurance risk and repair history...'):
+                    try:
+                        my_crew_instance = Mycrew()
+                        crew_instance = my_crew_instance.crew()
+                        crew_result = crew_instance.kickoff(inputs=inputs)
+
+                        st.subheader('🚘 Your Personalized Car Recommendations')
+                        st.markdown(crew_result)
+
+                        st.write('---')
+                        st.header('📊 Detailed Analysis Reports')
+
+                        files = [
+                            'affordability_analysis'
+                        ]
 
 
 if __name__=='__main__':
